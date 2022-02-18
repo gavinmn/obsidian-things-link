f99c1b450587491d80c7603068b40aa9c2b8ee9c
Gavin Nelson <gavin@nelson.co>
2022-02-18 14:54:48 -0800

    add task creation from current line

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		this.addCommand({
-			id: 'link-to-things',
-			name: 'Link to Things',
+			id: 'create-things-project',
+			name: 'Create Things Project',
 			checkCallback: (checking: boolean) => {
 				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (view) {
@@ -66,6 +66,70 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
+		this.addCommand({
+			id: 'create-things-task',
+			name: 'Create Things Task',
+			checkCallback: (checking: boolean) => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (view) {
+					if (!checking) {
+						const vault = this.app.vault;
+						const workspace = this.app.workspace;
+					
+						
+						function getCurrentLine(){
+							const lineNumber = view.editor.getCursor().line
+							const lineText = view.editor.getLine(lineNumber)
+							return lineText
+						}
+
+						function prepareLine(line:string) {
+							line = line.replace(/^-/gm, '')
+							line = line.replace(/^\*/gm, '')
+							line = line.replace(/^#/gm, '')
+							line = line.replace(/^_/gm, '')
+							line = line.replace(/^>/gm, '')
+							line = line.replace(/^\t/gm, '')
+							line = line.trim()
+							return line
+						}
+
+						function encodeLine(line: string) {
+							line = line.replace(/\s/g, '%20')
+							line = line.replace(/\:/g, '%3A')
+							line = line.replace(/\//g, '%2F')
+							line = line.replace(/\?/g, '%3F')
+							line = line.replace(/\=/g, '%3D')
+							line = line.replace(/\#/g, '%23')
+							line = line.replace(/\@/g, '%40')
+							line = line.replace(/\$/g, '%24')
+							line = line.replace(/\^/g, '%5E')
+							line = line.replace(/\&/g, '%26')
+							line = line.replace(/\+/g, '%2B')
+							line = line.replace(/\:/g, '%3A')
+							line = line.replace(/\;/g, '%3B')
+							line = line.replace(/\</g, '%3C')
+							line = line.replace(/\>/g, '%3E')
+							return line
+						}
+
+						function createTask(line: string) {
+							const task = `things:///add?title=${line}`
+							console.log(task)
+							window.open(task);
+						}
+
+						const task = getCurrentLine()
+						const cleanedLine = prepareLine(task)
+						const encodedLine = encodeLine(cleanedLine)
+						createTask(encodedLine)
+					
+					}
+					return true
+				}
+				return false;
+			}
+		});
 	}
 
 	onunload() {
